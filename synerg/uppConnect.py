cfaee6e259b808f03ee47a6739ebb9d7607d6637
ivan-karaberov <karaberovivan123456@gmail.com>
2023-11-18 14:10:30 +0300

    feat: add function for send data from extension

--- synerg/uppConnect.py
+++ synerg/uppConnect.py
@@ -1,12 +1,18 @@
-import time
-
 import requests
-
+import json
 
 def get_id():
     '''Получение id-паллета с системы upp'''
-    request = requests.get("http://127.0.0.1:8000/pallets")
+    request = requests.get("http://127.0.0.1:5000/pallets")
     id = []
     if request.status_code == 200:
-        id = request.json()["buffer_zone"]
+        id = request.json()["id"]
     return id
+
+
+def send_id():
+    '''Полученные данные отправляем POST'''
+    data = get_id()
+    if len(data):
+        params={"data": data}
+        requests.post("http://127.0.0.1:8000/send_id", json=params)
